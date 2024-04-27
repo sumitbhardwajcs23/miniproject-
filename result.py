d4d211a73dbc28bc45808b149f1832b41a217f0b
sumitbhardwajcs23 <168277673+sumitbhardwajcs23@users.noreply.github.com>
2024-04-27 18:21:27 +0530

    Add files via upload

--- result.py
+++ result.py
@@ -1,28 +1,26 @@
-#RESULT using  if 
-name=input("enter your name ")
-standared=input("enter your class ")
-roll=input("enter your roll number")
-maths=int(input("enter your math mask"))
-science=int(input("enter your maths mask "))
-hindi=int(input("enter your hindi mask"))
-english=int(input("enter your english mask"))
-total=maths+science+hindi+english
-percentage=total/4
-if percentage>=33 and hindi>=33:
-    if science>=33  and hindi>=33:
-        if maths>=33 :
-             print("pass")
-             print ("congrats you got ",percentage)
-else :
-    print("try nest time")
-    print("paisa barbaad b***** ",percentage)
-
-
-
-
-
-
-
-
-
-
+#result 
+name=input("enter the name of student ")
+s1=int(input("enter the first subject marks "))
+s2=int(input("enter the second subject marks "))
+s3=int(input("enter the marks of third subject "))
+s4=int(input("enter the marks of forth subject "))
+s5=int(input("enter the marks of fifth subject "))
+t=s1+s2+s3+s4+s5
+percentage=t/5
+print("your percentage is:",percentage)
+if(s1>100 or s2>100 or s3>100 or s4>100 or s5>100 or s1<0 or s2<0 or s3<0 or s4<0 or s5<0):
+    print("enter the wrong marks criteria")
+elif(percentage==100):
+    print("grade==O")
+elif(percentage>=90):
+    print("grade==A+")
+elif(percentage>=80):
+    print("grade==B+")
+elif(percentage>=70):
+    print("grade==B")
+elif(percentage>=60):
+    print("grade==C")
+elif(percentage>=50):
+    print("grade==D")
+else:
+    print("the student is fail")
